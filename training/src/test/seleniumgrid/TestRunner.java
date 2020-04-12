29da3a9807416a1666b4e574340ca56fd0b386a7
shavantha <shavantha@gmail.com>
2020-04-12 19:28:52 +0530

    selenium grid example

---
+++ training/src/test/seleniumgrid/TestRunner.java
@@ -0,0 +1,12 @@
+package seleniumgrid;
+
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.*;
+
+
+    @CucumberOptions(features="/Users/shavanthaweerasinghe/projects/training/training/src/test/java/seleniumgrid",
+            glue="/Users/shavanthaweerasinghe/projects/training/training/src/test/java/seleniumgrid")
+    public class TestRunner {
+
+    }
+
